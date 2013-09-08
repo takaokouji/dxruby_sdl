c1d467ad847ca1bcceca4cd170188534c1c7ff4f
Kouji Takao <kouji.takao@gmail.com>
2013-09-08 17:18:32 +0900

    added DXRubySDL::Image.new skelton.

--- lib/dxruby_sdl.rb
+++ lib/dxruby_sdl.rb
@@ -3,6 +3,7 @@
 require 'dxruby_sdl/version'
 require 'sdl'
 require 'dxruby_sdl/window'
+require 'dxruby_sdl/image'
 
 module DXRubySDL
 end
--- lib/dxruby_sdl/image.rb
+++ lib/dxruby_sdl/image.rb
@@ -2,7 +2,13 @@
 
 module DXRubySDL
   class Image
+    attr_reader :width
+    attr_reader :height
+    
     def initialize(width, height, color = [0, 0, 0, 0])
+      @width = width
+      @height = height
+      @color = color
     end
   end
 end
