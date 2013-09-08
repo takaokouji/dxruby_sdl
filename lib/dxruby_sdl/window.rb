56b29dee9c954beaf6abb5d6cac8187219539100
Kouji Takao <kouji.takao@gmail.com>
2013-09-08 16:05:11 +0900

    implemented Window.loop {}.

--- lib/dxruby_sdl/window.rb
+++ lib/dxruby_sdl/window.rb
@@ -1,4 +1,40 @@
+require 'dxruby_sdl/window/fpstimer'
+
 module DXRubySDL
-  class Window
+  module Window
+    module_function
+
+    def loop(&block)
+      SDL.init(SDL::INIT_EVERYTHING)
+      screen = SDL.set_video_mode(DEFAULTS[:width], DEFAULTS[:height], 16, SDL::SWSURFACE)
+      screen.fill_rect(0, 0, DEFAULTS[:width], DEFAULTS[:height], DEFAULTS[:background_color])
+      screen.update_rect(0, 0, 0, 0)
+
+      timer = FPSTimer.new
+      timer.reset
+      begin
+        Kernel.loop do
+          while (event = SDL::Event.poll)
+            case event
+            when SDL::Event::Quit
+              exit
+            end
+          end
+          timer.wait_frame do
+            yield
+          end
+        end
+      rescue SystemExit
+      end
+    end
+
+    private
+
+    DEFAULTS = {
+      width: 640,
+      height: 480,
+      background_color: [0, 0, 0],
+    }
+    private_constant :DEFAULTS
   end
 end
--- spec/dxruby_sdl/window_spec.rb
+++ spec/dxruby_sdl/window_spec.rb
@@ -15,20 +15,8 @@ describe DXRubySDL::Window do
     
     describe '.loop', 'メインループ' do
       it "サイズが#{default[:width]}x#{default[:height]}、背景がRGB(#{default[:background_color].join(", ")})のウィンドウを表示して、ESCキーを入力するまで待つ" do
-        SDL.init(SDL::INIT_EVERYTHING)
-        screen = SDL.set_video_mode(width, height, 16, SDL::SWSURFACE)
-        screen.fill_rect(0, 0, width, height, background_color)
-        screen.update_rect(0, 0, 0, 0)
-        begin
-          loop do  
-            while (event = SDL::Event2.poll)
-              case event
-              when SDL::Event2::Quit
-                exit
-              end
-            end
-          end
-        rescue SystemExit
+        DXRubySDL::Window.loop do
+          SDL::Event.push(SDL::Event::Quit.new)
         end
       end
     end
