a9084cbf9131ebfb2ad09c89af7c25be7a9a6be4
Kouji Takao <kouji.takao@gmail.com>
2013-09-09 19:42:00 +0900

    supported DXRubySDL::Input.pad_down?.

--- lib/dxruby_sdl/input.rb
+++ lib/dxruby_sdl/input.rb
@@ -5,27 +5,57 @@ module DXRubySDL
     module_function
 
     def x(pad_number = 0)
-      SDL::Key.scan
       res = 0
-      if SDL::Key.press?(SDL::Key::LEFT)
+      if key_press?(SDL::Key::LEFT)
         res -= 1
       end
-      if SDL::Key.press?(SDL::Key::RIGHT)
+      if key_press?(SDL::Key::RIGHT)
         res += 1
       end
       return res
     end
 
     def y(pad_number = 0)
-      SDL::Key.scan
       res = 0
-      if SDL::Key.press?(SDL::Key::UP)
+      if key_press?(SDL::Key::UP)
         res -= 1
       end
-      if SDL::Key.press?(SDL::Key::DOWN)
+      if key_press?(SDL::Key::DOWN)
         res += 1
       end
       return res
+    end
+
+    def pad_down?(button_code, pad_number = 0)
+      if key_press?(SDL::Key::Z) ||
+          ((j = joystick(pad_number)) && j.button(button_code))
+        return true
+      end
+      return false
+    end
+
+    private
+
+    @joysticks = []
+
+    class << self
+
+      private
+
+      def key_press?(key)
+        SDL::Key.scan
+        return SDL::Key.press?(key)
+      end
+
+      def joystick(pad_number)
+        if pad_number >= SDL::Joystick.num
+          return nil
+        end
+        if !@joysticks[pad_number]
+          @joysticks[pad_number] = SDL::Joystick.open(pad_number)
+        end
+        return @joysticks[pad_number]
+      end
     end
   end
 end
--- spec/dxruby_sdl/input_spec.rb
+++ spec/dxruby_sdl/input_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 
 describe DXRubySDL::Input,
          'キーボード・ゲームパッド・マウスの入力を扱うモジュール' do
-  def push_key(*keys)
-    allow(SDL::Key).to receive(:press?) { |key|
-                         keys.include?(key) ? true : false
-                       }
+  after do
+    DXRubySDL::Input.instance_variable_set('@joysticks', [])
   end
 
   shared_context 'push_key' do
@@ -14,7 +12,9 @@ describe DXRubySDL::Input,
 
     before do
       allow(SDL::Key).to receive(:scan)
-      push_key(*_keys)
+      allow(SDL::Key).to receive(:press?) { |key|
+                           [_keys].flatten.include?(key) ? true : false
+                         }
     end
   end
 
@@ -71,6 +71,69 @@ describe DXRubySDL::Input,
 
     context '上・下カーソルのどちらも押されていない場合' do
       it { should eq(0) }
+    end
+  end
+
+  describe '.pad_down?', 'パッドのボタン状態を返す' do
+    include_context 'push_key'
+
+    context 'Joystickが接続されている場合' do
+      let(:joystick) {
+        j = double('Joystick')
+        allow(j).to receive(:button).with(anything).and_return(false)
+        j
+      }
+
+      before do
+        allow(SDL::Joystick).to receive(:num).and_return(1)
+        allow(SDL::Joystick).to receive(:open).with(0).and_return(joystick)
+      end
+
+      describe '0番目(P_BUTTON0)のボタン' do
+        subject { described_class.pad_down?(DXRubySDL::P_BUTTON0) }
+
+        context '0番目のボタンが押されている場合' do
+          let(:joystick) {
+            j = double('Joystick')
+            allow(j).to receive(:button) { |button_index|
+                          button_index == DXRubySDL::P_BUTTON0
+                        }
+            j
+          }
+
+          it { should be_true }
+        end
+
+        context 'Zキーが押されている場合' do
+          let(:_keys) { SDL::Key::Z }
+
+          it { should be_true }
+        end
+
+        context 'ボタンやキーが押されていない場合' do
+          it { should be_false }
+        end
+      end
+    end
+
+    context 'Joystickが接続されていない場合' do
+      before do
+        allow(SDL::Joystick).to receive(:num).and_return(0)
+      end
+
+      describe '0番目(P_BUTTON0)のボタン' do
+        subject { described_class.pad_down?(DXRubySDL::P_BUTTON0) }
+
+        context 'Zキーが押されている場合' do
+          let(:_keys) { SDL::Key::Z }
+
+          it { should be_true }
+        end
+
+        context 'ボタンやキーが押されていない場合' do
+          it { should be_false }
+        end
+      end
     end
   end
 end
