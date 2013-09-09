505c30fb428ff70fc06520736686d5e1f9f5c6b4
Kouji Takao <kouji.takao@gmail.com>
2013-09-10 00:17:47 +0900

    supported DXRubySDL::Sound#play.

--- lib/dxruby_sdl/sound.rb
+++ lib/dxruby_sdl/sound.rb
@@ -2,7 +2,46 @@
 
 module DXRubySDL
   class Sound
+    @sdl_mixer_openend = false
+
     def initialize(filename)
+      if !self.class.instance_variable_get('@sdl_mixer_openend')
+        SDL::Mixer.open
+        self.class.instance_variable_set('@sdl_mixer_openend', true)
+      end
+      if /\.mid$/ =~ filename
+        @sound = Music.new(filename)
+      else
+        @sound = Wave.new(filename)
+      end
     end
+
+    def play
+      @sound.play
+    end
+
+    private
+
+    class Music
+      def initialize(filename)
+        @music = SDL::Mixer::Music.load(filename)
+      end
+
+      def play
+        SDL::Mixer.play_music(@music, -1)
+      end
+    end
+    private_constant :Music
+
+    class Wave
+      def initialize(filename)
+        @wave = SDL::Mixer::Wave.load(filename)
+      end
+
+      def play
+        SDL::Mixer.play_channel(-1, @wave, 0)
+      end
+    end
+    private_constant :Wave
   end
 end
--- spec/dxruby_sdl/sound_spec.rb
+++ spec/dxruby_sdl/sound_spec.rb
@@ -23,4 +23,34 @@ describe DXRubySDL::Sound, '音を表すクラス' do
       include_context '.new'
     end
   end
+
+  describe '#play' do
+    context 'WAVE形式のファイルの場合' do
+      let(:path) { fixture_path('sound.wav') }
+      let(:sound) { DXRubySDL::Sound.new(path) }
+
+      subject { sound.play }
+
+      it 'SDL::Mixer.play_channelを呼び出す' do
+        wave =
+          sound.instance_variable_get('@sound').instance_variable_get('@wave')
+        expect(SDL::Mixer).to receive(:play_channel).with(-1, wave, 0)
+        subject
+      end
+    end
+
+    context 'MIDI形式のファイルの場合' do
+      let(:path) { fixture_path('bgm.mid') }
+      let(:sound) { DXRubySDL::Sound.new(path) }
+
+      subject { sound.play }
+
+      it 'SDL::Mixer.play_musicを呼び出す' do
+        music =
+          sound.instance_variable_get('@sound').instance_variable_get('@music')
+        expect(SDL::Mixer).to receive(:play_music).with(music, -1)
+        subject
+      end
+    end
+  end
 end
