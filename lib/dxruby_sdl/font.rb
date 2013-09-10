4150f2175e4467ad3e09296e54a649298e256f05
Kouji Takao <kouji.takao@gmail.com>
2013-09-11 04:10:19 +0900

    cached loaded SDL::TTF.

--- lib/dxruby_sdl/font.rb
+++ lib/dxruby_sdl/font.rb
@@ -5,6 +5,8 @@ module DXRubySDL
     attr_reader :size
     attr_reader :_ttf
 
+    @font_cache = {}
+
     def initialize(size, fontname = '', hash = {})
       @size = size
 
@@ -14,7 +16,13 @@ module DXRubySDL
       if !(path = FONTS[fontname.downcase])
         path = FONTS.first.last
       end
-      @_ttf = SDL::TTF.open(path, size)
+      font_cache = Font.instance_variable_get('@font_cache')
+      args = [path, size]
+      if font_cache.key?(args)
+        @_ttf = font_cache[args]
+      else
+        @_ttf = font_cache[args] = SDL::TTF.open(*args)
+      end
     end
 
     private
