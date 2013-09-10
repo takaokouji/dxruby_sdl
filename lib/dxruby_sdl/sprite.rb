02c0e7a3f301a8b0ee6f822e882ac21dbdde2932
Kouji Takao <kouji.takao@gmail.com>
2013-09-11 02:46:11 +0900

    added Sprite's methods skelton.

--- lib/dxruby_sdl/sprite.rb
+++ lib/dxruby_sdl/sprite.rb
@@ -5,11 +5,62 @@ module DXRubySDL
     attr_accessor :x
     attr_accessor :y
     attr_accessor :image
+    attr_accessor :angle
+    attr_accessor :scale_x
+    attr_accessor :scale_y
+    attr_accessor :center_x
+    attr_accessor :center_y
+    attr_accessor :alpha
+    attr_accessor :blend
+    attr_accessor :shader
+    attr_accessor :target
+    attr_accessor :collision
+    attr_accessor :collision_enable
+    attr_accessor :collision_sync
+    attr_accessor :visible
+
+    class << self
+      def check(o, d, shot = :shot, hit = :hit)
+      end
+
+      def update(ary)
+      end
+
+      def draw(ary)
+      end
+
+      def clean(ary)
+      end
+    end
 
     def initialize(x = 0, y = 0, image = nil)
       @x = x
       @y = y
       @image = image
+
+      @collision_enable = true
+      @collision_sync = true
+      @visible = true
+      @vanished = false
+    end
+
+    def draw
+    end
+
+    def ===(other)
+      return true
+    end
+
+    def check(s)
+      return []
+    end
+
+    def vanish
+      @vanished = true
+    end
+
+    def vanished?
+      return @vanished
     end
   end
 end
