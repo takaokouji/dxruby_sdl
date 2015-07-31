af4c77a7ebc7ea9c84557de3e78f9b608374e552
kawasakikou <forte0801@gmail.com>
2015-07-31 10:49:28 +0900

    Added read _surface

--- lib/dxruby_sdl/render_target.rb
+++ lib/dxruby_sdl/render_target.rb
@@ -6,8 +6,11 @@ module DXRubySDL
   class RenderTarget
     extend Forwardable
 
+    attr_reader :_surface
+
     def initialize(width, height, color)
       @image = Image.new(width, height, color)
+      @_surface = @image._surface
     end
     
     def draw_font(x, y, string, font, option = {})
