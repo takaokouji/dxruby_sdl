7526ed02e457ba26b90848a07e07155f5a9ea84f
kawasakikou <forte0801@gmail.com>
2015-07-31 13:12:06 +0900

    fix line of def_deligators and to draw_font

--- lib/dxruby_sdl/render_target.rb
+++ lib/dxruby_sdl/render_target.rb
@@ -12,14 +12,15 @@ module DXRubySDL
       @image = Image.new(width, height, color)
       @_surface = @image._surface
     end
+
+    def_delegators :@image, :draw
     
     def draw_font(x, y, string, font, option = {})
       color = option[:color] || [255, 255, 255]
-      @image.drawFont(x, y, string, font, color) 
+      @image.draw_font(x, y, string, font, color) 
     end
 
-    def_delegators :@image, :draw
     alias_method :drawFont, :draw_font
-  end
 
+  end
 end
