94182061fdf3dee5f4c80c3a46fdf2ba57eb8f51
Kouji Takao <kouji.takao@gmail.com>
2017-01-05 00:06:54 +0900

    suppressed deprecation warnings for RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@
 require 'coveralls'
 Coveralls.wear!
 
+require 'rspec/its'
+require 'rspec/collection_matchers'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dxruby_sdl'
 
